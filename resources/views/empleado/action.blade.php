a305aab87c231f127fa203c9ae0fb3bfc45b7924
Collins Vieira <vabadcollinspoo@uss.edu.pe>
2024-11-04 16:41:40 -0500

    solucionado el error con el formulario empleados

--- resources/views/empleado/action.blade.php
+++ resources/views/empleado/action.blade.php
@@ -5,21 +5,27 @@
             <span aria-hidden="true">&times;</span>
         </button>
     </div>
-    <form id="empleadoForm" action="{{ isset($empleado->id) ? route('empleado.update', $empleado->id) : route('empleado.store') }}"
+    <form id="empleadoForm"
+        action="{{ isset($empleado->id) ? route('empleado.update', $empleado->id) : route('empleado.store') }}"
         method="POST" enctype="multipart/form-data">
         @csrf
-        @if(isset($empleado->id))
-        @method('PUT')
+        @if (isset($empleado->id))
+            @method('PUT')
         @endif
         <div class="modal-body">
             <!-- Datos de Usuario -->
             <div class="row">
                 <div class="row mb-4">
                     <div class="col-md-12 text-center">
-                        <img id="preview"
-                            src="{{ $empleado->imagen_perfil ? asset('empleados/'.$empleado->imagen_perfil) : asset('img/user-avatar.png') }}"
-                            class="rounded-circle border shadow-sm"
-                            style="width: 120px; height: 120px; object-fit: cover;">
+                        @if (isset($empleado) && $empleado->imagen_perfil)
+                            <img id="preview" src="{{ asset('empleados/' . $empleado->imagen_perfil) }}"
+                                class="rounded-circle border shadow-sm"
+                                style="width: 120px; height: 120px; object-fit: cover;">
+                        @else
+                            <img id="preview" src="{{ asset('perfiles/user-avatar.png') }}"
+                                class="rounded-circle border shadow-sm"
+                                style="width: 120px; height: 120px; object-fit: cover;">
+                        @endif
 
                         <div class="mt-3">
                             <div class="custom-file" style="max-width: 300px;">
@@ -49,30 +55,30 @@
                 </div>
             </div>
 
-            @if(!isset($empleado->id))
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="usuario">Usuario</label>
-                        <input type="text" class="form-control" id="usuario" name="usuario" required>
-                        <small class="text-danger"></small>
+            @if (!isset($empleado->id))
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="usuario">Usuario</label>
+                            <input type="text" class="form-control" id="usuario" name="usuario" required>
+                            <small class="text-danger"></small>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email" name="email" required>
+                            <small class="text-danger"></small>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="password">Contraseña</label>
+                            <input type="password" class="form-control" id="password" name="password" required>
+                            <small class="text-danger"></small>
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" name="email" required>
-                        <small class="text-danger"></small>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="password">Contraseña</label>
-                        <input type="password" class="form-control" id="password" name="password" required>
-                        <small class="text-danger"></small>
-                    </div>
-                </div>
-            </div>
             @endif
 
             <!-- Datos de Empleado -->
